057a66c1093756c9e8e5ad8cac63510148e73b4f
Srujan <crazycoder0407@gmail.com>
2025-03-08 17:13:12 +0530

    discount added

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -4,11 +4,12 @@ import { BsFillStarFill } from "react-icons/bs";
 
 export default function ProductCard({ product }) {
   const [isFavorite, setIsFavorite] = useState(false);
-
+  const percentage_discount=Math.ceil(((product.original_price-product.price)/product.original_price)*100);
+  
   return (
     <div className="bg-[#02042B] p-4 rounded-xl shadow-lg w-64 border border-gray-700 text-white">
       {/* Product Image & Favorite Button */}
-      <div className="relative">
+      <div className="relative hover:cursor-pointer">
         <img
           src={product.image}
           alt={product.name}
@@ -25,20 +26,36 @@ export default function ProductCard({ product }) {
           )}
         </button>
       </div>
-
-      {/* Product Details */}
-      <div className="mt-3 text-center">
-        <h2 className="text-lg font-bold text-white">{product.name}</h2>
-        <p className="text-gray-300 font-semibold">₹ {product.price}/-</p>
-
-        {/* Star Ratings */}
-        <div className="flex justify-center my-2 text-yellow-500">
+      {/* Star Ratings */}
+      <div className="flex justify-left my-2 text-yellow-500">
           {Array(5)
             .fill(0)
             .map((_, i) => (
               <BsFillStarFill key={i} className="mx-0.5" />
             ))}
+          
         </div>
+
+      {/* Product Details */}
+      <div className="mt-3 text-center">
+        <h2 className="text-lg font-bold text-left text-white">{product.name}</h2>
+        <div className="flex items-center gap-2">
+          {(product.original_price>product.price) ?(
+          <>
+            <p className="text-blue-200 text-1xl line-through font-semibold">₹ {product.original_price}/- </p>
+            <p className="text-blue-300 text-2xl text-left font-semibold">₹ {product.price}/- </p>
+            <p className="text-red-400 text-1xl font-semibold" > {percentage_discount}% off </p>
+          </>):
+          (
+          <>
+            <p className="text-blue-300 text-2xl font-semibold">₹ {product.original_price}/- </p>
+          </>)}
+          
+         
+        </div>
+       
+        
+        
 
         {/* Buttons */}
         <div className="flex gap-2 mt-3">
